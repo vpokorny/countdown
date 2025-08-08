51d18b174592440bcfada21440b4a937a67a52d2
vaclav_pokorny <pokorny.vena@gmail.com>
2025-08-08 19:10:14 +0200

    Add circle to the counter windows instead of just the text

--- src/js/common.js
+++ src/js/common.js
@@ -3,8 +3,8 @@
 // HTML Time visualization
 const SEQUENCE_ID = 'sequenceView';
 const SPEAKER_MESSAGE = {
-    stop: 'STOP',
-    chill: 'CHILL ;-)'
+    stop: 'Time\'s up ⌛',
+    chill: 'Relax 👌'
 };
 
 // HTML IDs of time form fields for hours, minutes and seconds
@@ -40,12 +40,12 @@ function transform2TimeString(time) {
     return `${hours}:${minutes}:${seconds}`;
 }
 
-// Function that visualizes the counter
+// Control panel: Function that visualizes the counter
 function setCounter(document, id, message) {
     document.getElementById(id).innerText = message;
 }
 
-// Function that visualizes the sequence list
+// Control panel: Function that visualizes the sequence list
 function setSequence(document, timer) {
     let res = ``;
     for (let i = 0; i < timer.sequence.length; i++) {
@@ -65,7 +65,7 @@ function setSequence(document, timer) {
     document.getElementById(SEQUENCE_ID).innerHTML = res;
 }
 
-// Function that runs each second to update visualized global counter variable (controlTime) value
+// Control panel: Function that runs each second to update visualized global counter variable (controlTime) value
 function countDown(document, id, timeDict) {
     if (timeDict.counter === 0) {
         setCounter(document, id, SPEAKER_MESSAGE.stop);
@@ -82,9 +82,120 @@ function countDown(document, id, timeDict) {
     }
 }
 
+// Counter: Function that picks the color based on provided percentage of counter / or special cases
+function pickColorByPercentage(percentage) {
+    // The percentage is above 100 % (special case)
+    if (percentage > 100) {
+        return '#BBBBBB'; // Grey
+
+    // The counter above between 100 - 50 % of the countdown duration.
+    } else if (percentage >= 50) {
+         return '#28a745'; // Green
+
+    // The counter is between 25 - 50 % of the countdown duration.
+    } else if (percentage >= 25) {
+        return '#17a2b8'; // Blue
+
+    // The counter is between 10 - 25 % of the countdown duration.
+    } else if (percentage >= 10) {
+        return '#ffc107'; // Orange
+
+    // The counter is under the 10% of the countdown duration + Time's up.
+    } else if (percentage >= 0) {
+        return '#dc3545'; // Red
+
+    // Visualization of just text out of the counter (like percentage === -1, etc.) (special case)
+    } else {
+        return '#BBBBBB'; // Grey
+    }
+};
+
+// Counter: Function that redraws the circle every second
+function countDownCircle(document, id, timeDict) {
+    // Get the canvas element that will be adjusted every second
+    const canvas = document.getElementById(id);
+
+    // Scale the canvas to fill the full height of the counter window
+    const size = window.innerHeight;
+
+    // Respect the device pixel ratio of High Resolution screens
+    const dpr = window.devicePixelRatio || 1;
+
+    // Adjust the size of the canvas respecting the device pixel ratio for high resolution screens
+    canvas.width = size * dpr;
+    canvas.height = size * dpr;
+
+    // Get the coordinates of the circle center
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+
+    // Sets the size of the windows with respect to the CSS
+    canvas.style.width = size + 'px';
+    canvas.style.height = size + 'px';
+
+    // Obtain the context that will be adjusted below
+    const ctx = canvas.getContext('2d');
+
+    // Clean the rectangle
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Configuration of placement of the text
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+
+    // Text size in [px]
+    const fontSize = Math.round(size / 7);
+    const font = `${fontSize}px Arial`;
+    ctx.font = font
+
+    // Time's up - Do not draw the circle, leave just the text.
+    if (timeDict.counter === 0) {
+        ctx.fillStyle = pickColorByPercentage(0);
+        ctx.fillText(SPEAKER_MESSAGE.stop, centerX, centerY)
+
+    // Running countdown - Draw the circle and the current timer.
+    } else if (timeDict.counter > 0) {
+        // The radius of the circle (inner or outer?) in the squared canvas, the range is (0, 0.5) * size of the square
+        const radius = 0.4 * size;
+
+        // Calculate how much time in percentage is remains in range <0, 1>
+        const percentage = timeDict.counter / timeDict.duration;
+
+        // Pick the color based on the percentage
+        const color = pickColorByPercentage(percentage * 100);
+
+        // Draw a new arc based on the percentage of remaining time
+        ctx.beginPath();
+        ctx.arc(centerX, centerY, radius, -Math.PI / 2, -Math.PI / 2 + (2 * Math.PI * percentage), false);
+        ctx.strokeStyle = color;
+        ctx.lineWidth = canvas.width / 10; // The recommended range is <100 (very slim stroke), 5 (very bold))
+        ctx.stroke();
+
+        // Visualize current counter time
+        ctx.fillStyle = pickColorByPercentage(percentage * 100);
+        ctx.fillText(transform2TimeString(timeDict.counter), centerX, centerY)
+
+        // Decrease counter by 1 seconds
+        timeDict.counter -= 1;
+
+    // Chill - Do not draw the circle, leave just the text.
+    } else if (timeDict.counter === -1) {
+        ctx.fillStyle = pickColorByPercentage(-1);
+        ctx.fillText(SPEAKER_MESSAGE.chill, centerX, centerY)
+
+    // Error state - leave just the text.
+    } else {
+        ctx.fillStyle = pickColorByPercentage(-1);
+        ctx.fillText(`Err: [${timeDict.counter}].`, centerX, centerY);
+    }
+}
+
+
+
 // Export functions
 module.exports = {
     countDown,
     transform2Seconds,
-    setSequence
+    setSequence,
+    countDownCircle,
 }
--- src/js/counter.js
+++ src/js/counter.js
@@ -5,16 +5,20 @@ const common = require('../js/common');
 
 // Constants
 const COUNTER_SHOW_ID = 'counterShow';
+const CANVAS_SHOW_ID = 'cvs';
 
 // Define the object (not a simple variable like number)
 let showTime = {
-    counter: 0
+    counter: 0,
+    duration: 0
 };
 ipcRenderer.on('action-set-counter', (event, arg) => {
     showTime.counter = Number(arg);
+    showTime.duration = Number(arg);
 })
 
 // Start the counter function
 setInterval(() => {
-    common.countDown(document, COUNTER_SHOW_ID, showTime);
+    // common.countDown(document, COUNTER_SHOW_ID, showTime);
+    common.countDownCircle(document, CANVAS_SHOW_ID, showTime)
 }, 1000);
